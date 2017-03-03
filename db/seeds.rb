c7c931f11a54fb8bd11abfd6cff40de703c52d5b
becckitt <hello@beccabarton.com>
2017-03-03 17:03:02 -0500

    add seed data for text site

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,124 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(
+  name: "The Ordinary Hyaluronic Acid 2% + B5",
+  comment:
+   "No noticeable difference, but it does feel nice to put on, and is pretty cheap ($6)",
+  effective: false,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/001/original/Screen_Shot_2017-03-02_at_11.34.22_PM.png"),
+)
+
+Product.create(
+  name: "Nude Advanced Renewal Overnight Repair Mask",
+  comment:
+   "Works really well, skin looks a lot brighter and more hydrated. good buy would buy again",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/003/original/Screen_Shot_2017-03-02_at_11.35.37_PM.png"),
+)
+
+Product.create(
+  name: "Avène Hydrance Optimale Hydrating Serum",
+  comment: "Somehow manages to make your skin look poreless and airbrushed",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/005/original/Screen_Shot_2017-03-02_at_11.35.05_PM.png"),
+)
+
+Product.create(
+  name: "Avène Antirougeurs Crème",
+  comment:
+   "Maybe just avoid anything anti-redness, it seems to ironically make skin more red.",
+  effective: false,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/005/original/Screen_Shot_2017-03-02_at_11.35.05_PM.png"),
+)
+
+Product.create(
+  name: "Glossier Milky Jelly Cleanser",
+  comment:
+   "I don't get the hype around this... it just kind of smears your makeup around yet also manages to somehow be pretty drying.",
+  effective: false,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/006/original/IMG_9870.JPG"),
+)
+
+Product.create(
+  name: "Bioderma Hydrabio Sèrum",
+  comment:
+   "Not the biggest change, but definitely makes skin look better after using overnight.",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/007/original/IMG_9865.JPG"),
+)
+
+Product.create(
+  name: "First Aid Beauty Ultra Repair Hydrating Serum",
+  comment: "Is this supposed to do something?",
+  effective: false,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/008/original/IMG_9864.JPG")
+)
+
+Product.create(
+  name: "Glossier Haloscope, Quartz",
+  comment:
+   "I want to like this for ease of use, but clogs pores and honestly is just the lightest touch of highlighter that ends up sliding down your face after half an hour",
+  effective: false,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/009/original/IMG_9854.JPG.jpeg")
+)
+
+Product.create(
+  name: "Cane + Austin 10% Retexture Pads",
+  comment:
+   "These do work really well, but I think next time choosing the 20% or 30% concentration would bring better results.",
+  effective: false,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/010/original/IMG_9861.JPG")
+)
+
+Product.create(
+  name: "Bioderma Hydrabio Crème",
+  comment:
+   "Damn this shit is thick and luxurious. Makes your skin look nice and hydrated and glowing in the morning.",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/011/original/IMG_9862.JPG")
+)
+
+Product.create(
+  name: "Glossier Moon Mask",
+  comment:
+   "This works great as both a thick overnight cream and as a quick mask. Definitely want to have on hand.",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/012/original/IMG_9863.JPG")
+)
+
+Product.create(
+  name: "Kiehl's Ultra Facial Cleanser",
+  comment:
+   "Best facial cleanser I've used — gets makeup off completely and doesn't dry you out",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/013/original/IMG_9869.JPG")
+)
+
+Product.create(
+  name: "Glossier Greens Galaxy Mask",
+  comment:
+   "Nice detoxifying mask with weird solid bits, feels like it refreshes your skin.",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/014/original/IMG_9863.JPG")
+)
+
+Product.create(
+  name: "Avène Skin Recovery Cream",
+  comment:
+   "Works really well and also manages to make your skin look hydrated and poreless, like you got a mini-facial",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/015/original/Screen_Shot_2017-03-02_at_11.35.05_PM.png")
+)
+
+Product.create(
+  name: "Glossier Balm Dotcom, regular",
+  comment: "Best hydrating lip product I've used",
+  effective: true,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/016/original/Screen_Shot_2017-03-03_at_11.49.57_AM.png")
+)
+
+Product.create(
+  name: "Glossier Balm Dotcom, Mint",
+  comment:
+   "Something in this (probably the mint) is irritating and sometimes gives an allergic reaction around the mouth.",
+  effective: false,
+  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/017/original/Screen_Shot_2017-03-03_at_11.49.57_AM.png")
+)
