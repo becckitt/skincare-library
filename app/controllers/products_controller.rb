157c9553532064e59a6f69e7aa146ffc339c60f2
becckitt <hello@beccabarton.com>
2017-03-03 00:01:14 -0500

    add file upload and a bit of styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,6 +36,6 @@ class ProductsController < ApplicationController
 
   private
     def product_params
-      params.require(:product).permit(:name, :comment, :effective)
+      params.require(:product).permit(:name, :comment, :effective, :photo)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303004657) do
+ActiveRecord::Schema.define(version: 20170303041236) do
 
   create_table "products", force: :cascade do |t|
     t.text     "name"
     t.text     "comment"
     t.boolean  "effective"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
