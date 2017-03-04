154132328ea95f82e6c345cd733f51ef0a62f10b
becckitt <hello@beccabarton.com>
2017-03-04 10:16:47 -0500

    change image resize settings to handle non square images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_attached_file :photo, styles: { medium: "300x300#", thumb: "100x100#"}
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100#"}
   validates_attachment :photo, content_type: {content_type: ["image/jpg", "image/jpeg", "image/png"]}
   class << self
     def good_products
