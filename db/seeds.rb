c4dd09b0e334042c1b156680c15e7c5cf8829df2
becckitt <hello@beccabarton.com>
2017-03-06 00:04:07 -0500

    maybe this time it will work

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Product.create(
   comment:
    "No noticeable difference, but it does feel nice to put on, and is pretty cheap ($6)",
   effective: false,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/001/original/Screen_Shot_2017-03-02_at_11.34.22_PM.png"),
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/081/medium/Screen_Shot_2017-03-02_at_11.34.22_PM.png?1488641199"),
 )
 
 Product.create(
@@ -11,14 +11,14 @@ Product.create(
   comment:
    "Works really well, skin looks a lot brighter and more hydrated. good buy would buy again",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/003/original/Screen_Shot_2017-03-02_at_11.35.37_PM.png"),
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/082/medium/Screen_Shot_2017-03-02_at_11.35.37_PM.png?1488641138"),
 )
 
 Product.create(
   name: "Avène Hydrance Optimale Hydrating Serum",
   comment: "Somehow manages to make your skin look poreless and airbrushed",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/005/original/Screen_Shot_2017-03-02_at_11.35.05_PM.png"),
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/083/medium/Screen_Shot_2017-03-02_at_11.35.05_PM.png?1488640936"),
 )
 
 Product.create(
@@ -26,7 +26,7 @@ Product.create(
   comment:
    "Maybe just avoid anything anti-redness, it seems to ironically make skin more red.",
   effective: false,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/005/original/Screen_Shot_2017-03-02_at_11.35.05_PM.png"),
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/083/medium/Screen_Shot_2017-03-02_at_11.35.05_PM.png?1488640936"),
 )
 
 Product.create(
@@ -42,14 +42,14 @@ Product.create(
   comment:
    "Not the biggest change, but definitely makes skin look better after using overnight.",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/007/original/IMG_9865.JPG"),
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/086/medium/IMG_9865.JPG?1488641116"),
 )
 
 Product.create(
   name: "First Aid Beauty Ultra Repair Hydrating Serum",
   comment: "Is this supposed to do something?",
   effective: false,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/008/original/IMG_9864.JPG")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/087/medium/IMG_9864.JPG?1488641216")
 )
 
 Product.create(
@@ -57,7 +57,7 @@ Product.create(
   comment:
    "I want to like this for ease of use, but clogs pores and honestly is just the lightest touch of highlighter that ends up sliding down your face after half an hour",
   effective: false,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/009/original/IMG_9854.JPG.jpeg")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/088/medium/IMG_9854.JPG.jpeg?1488641177")
 )
 
 Product.create(
@@ -65,7 +65,7 @@ Product.create(
   comment:
    "These do work really well, but I think next time choosing the 20% or 30% concentration would bring better results.",
   effective: false,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/010/original/IMG_9861.JPG")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/089/medium/IMG_9861.JPG?1488641233")
 )
 
 Product.create(
@@ -73,7 +73,7 @@ Product.create(
   comment:
    "Damn this shit is thick and luxurious. Makes your skin look nice and hydrated and glowing in the morning.",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/011/original/IMG_9862.JPG")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/090/medium/IMG_9862.JPG?1488641096")
 )
 
 Product.create(
@@ -81,7 +81,7 @@ Product.create(
   comment:
    "This works great as both a thick overnight cream and as a quick mask. Definitely want to have on hand.",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/012/original/IMG_9863.JPG")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/093/medium/IMG_9863.JPG?1488640977")
 )
 
 Product.create(
@@ -89,7 +89,7 @@ Product.create(
   comment:
    "Best facial cleanser I've used — gets makeup off completely and doesn't dry you out",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/013/original/IMG_9869.JPG")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/092/medium/IMG_9869.JPG?1488641068")
 )
 
 Product.create(
@@ -97,7 +97,7 @@ Product.create(
   comment:
    "Nice detoxifying mask with weird solid bits, feels like it refreshes your skin.",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/014/original/IMG_9863.JPG")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/093/medium/IMG_9863.JPG?1488640977")
 )
 
 Product.create(
@@ -105,14 +105,14 @@ Product.create(
   comment:
    "Works really well and also manages to make your skin look hydrated and poreless, like you got a mini-facial",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/015/original/Screen_Shot_2017-03-02_at_11.35.05_PM.png")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/083/medium/Screen_Shot_2017-03-02_at_11.35.05_PM.png?1488640936")
 )
 
 Product.create(
   name: "Glossier Balm Dotcom, regular",
   comment: "Best hydrating lip product I've used",
   effective: true,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/016/original/Screen_Shot_2017-03-03_at_11.49.57_AM.png")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/095/medium/Screen_Shot_2017-03-03_at_11.49.57_AM.png?1488640909")
 )
 
 Product.create(
@@ -120,5 +120,5 @@ Product.create(
   comment:
    "Something in this (probably the mint) is irritating and sometimes gives an allergic reaction around the mouth.",
   effective: false,
-  photo: File.new("#{Rails.root}/public/system/products/photos/000/000/017/original/Screen_Shot_2017-03-03_at_11.49.57_AM.png")
+  photo: File.new("http://s3.amazonaws.com/skincare-index/products/photos/000/000/095/medium/Screen_Shot_2017-03-03_at_11.49.57_AM.png?1488640909")
 )
