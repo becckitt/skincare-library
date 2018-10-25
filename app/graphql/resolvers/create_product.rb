66fb7ece1cadf570b25022e5c052be1529ff0d20
Becca Barton <becca.barton@voxmedia.com>
2018-10-25 17:02:07 -0400

    add createproduct mutation to backend

--- app/graphql/resolvers/create_product.rb
+++ app/graphql/resolvers/create_product.rb
@@ -1,19 +1,31 @@
 class Resolvers::CreateProduct < GraphQL::Function
   argument :name, !types.String
-
+  argument :comment, !types.String
+  argument :price_cents, !types.String
+  argument :link, !types.String
+  argument :rating, !types.String
+  argument :repurchase, !types.String
+  argument :wishlist, !types.String
+  argument :brand, !types.String
+  argument :user, !types.String
+  
   type Types::ProductType
 
   def call(_obj, args, _ctx)
-    Product.create!(
+    brand = Brand.find_or_create_by!(name: args[:brand])
+    user = User.find_by(email: 'hello@beccabarton.com')
+
+    Product.find_or_create_by!(
       name:         args[:name],
       comment:      args[:comment],
       price_cents:  args[:price_cents],
       link:         args[:link],
+      rating:       args[:rating],
       repurchase:   args[:repurchase],
       product_type: args[:product_type],
       wishlist:     args[:wishlist],
-      brand:        args[:brand],
-      user:         _ctx[:current_user]
+      brand:        brand,
+      user:         user
     )
   rescue ActiveRecord::RecordInvalid => e
     GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")  
---
+++ app/skincare-frontend/src/views/CreateProduct.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="main">
+    <h1>Add a new product</h1>
+    <div class="details-section">
+      <div class="detail">
+        <!-- Name -->
+        <label for="product-name">Name:</label>
+        <input model="" id="product-name" placeholder="Product name">
+      </div>
+      <div class="detail">
+        <!-- Brand -->
+        <label for="brand-name">Brand:</label>
+        <input model="" id="brand-name" placeholder="Brand name">
+      </div>
+      <div class="detail">
+        <!-- Product type -->
+        <label for="product-type">Product type:</label>
+        <input model="" id="product-type" placeholder="Product type">
+      </div>
+      <div class="detail">
+        <!-- Price cents -->
+        <label for="price">Price:</label>
+        <input model="" id="price" placeholder="Price">
+      </div>
+      <div class="detail">
+        <!-- ingredients -->
+        <label for="ingredients">Ingredients:</label>
+        <input model="" id="ingredients" placeholder="Ingredients (separated by comma)">
+      </div>
+      <div class="detail">
+        <!-- tags -->
+        <label for="tags">Tags:</label>
+        <input model="" id="tags" placeholder="Tags (separated by comma)">
+      </div>
+      <!-- Wishlist boolean -->
+
+      <!-- DEPENDENT ON WISHLIST -->
+      <!-- rating -->
+      <!-- repurchase -->
+      <!-- comment -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateProduct'
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+.details-section {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  text-align: left;
+}
+
+.detail {
+  margin-bottom: 20px;
+}
+
+.detail.long-section {
+  grid-column: 1/2;
+}
+
+input {
+  border: none;
+  border-bottom: 2px black solid;
+  height: 30px;
+  width: 250px;
+}
+
+label {
+  font-weight: bold;
+  padding-right: 5px;
+}
+</style>
