06c6980cfaf6b94a0db550def2a9234f3a1f3b81
Becca Barton <becca.barton@voxmedia.com>
2018-10-26 22:24:14 -0400

    let users add new products

--- app/graphql/resolvers/create_product.rb
+++ app/graphql/resolvers/create_product.rb
@@ -2,6 +2,7 @@ class Resolvers::CreateProduct < GraphQL::Function
   argument :name, !types.String
   argument :comment, !types.String
   argument :price_cents, !types.String
+  argument :product_type, !types.String
   argument :link, !types.String
   argument :rating, !types.String
   argument :repurchase, !types.String
@@ -12,21 +13,21 @@ class Resolvers::CreateProduct < GraphQL::Function
   type Types::ProductType
 
   def call(_obj, args, _ctx)
-    brand = Brand.find_or_create_by!(name: args[:brand])
+    brand = Brand.find_or_create_by(name: args[:brand])
     user = User.find_by(email: 'hello@beccabarton.com')
+    product = Product.find_or_create_by(name: args[:name], brand: brand, user: user)
 
-    Product.find_or_create_by!(
-      name:         args[:name],
+    product.update_attributes(
       comment:      args[:comment],
+      product_type: args[:product_type].downcase,
       price_cents:  args[:price_cents],
       link:         args[:link],
       rating:       args[:rating],
-      repurchase:   args[:repurchase],
-      product_type: args[:product_type],
-      wishlist:     args[:wishlist],
-      brand:        brand,
-      user:         user
+      repurchase:   args[:repurchase].downcase,
+      wishlist:     args[:wishlist]
     )
+    
+    product
   rescue ActiveRecord::RecordInvalid => e
     GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")  
   end
--- app/skincare-frontend/src/views/CreateProduct.vue
+++ app/skincare-frontend/src/views/CreateProduct.vue
@@ -1,50 +1,142 @@
 <template>
   <div class="main">
     <h1>Add a new product</h1>
-    <div class="details-section">
-      <div class="detail">
-        <!-- Name -->
-        <label for="product-name">Name:</label>
-        <input model="" id="product-name" placeholder="Product name">
-      </div>
-      <div class="detail">
-        <!-- Brand -->
-        <label for="brand-name">Brand:</label>
-        <input model="" id="brand-name" placeholder="Brand name">
-      </div>
-      <div class="detail">
-        <!-- Product type -->
-        <label for="product-type">Product type:</label>
-        <input model="" id="product-type" placeholder="Product type">
-      </div>
-      <div class="detail">
-        <!-- Price cents -->
-        <label for="price">Price:</label>
-        <input model="" id="price" placeholder="Price">
-      </div>
-      <div class="detail">
-        <!-- ingredients -->
-        <label for="ingredients">Ingredients:</label>
-        <input model="" id="ingredients" placeholder="Ingredients (separated by comma)">
-      </div>
-      <div class="detail">
-        <!-- tags -->
-        <label for="tags">Tags:</label>
-        <input model="" id="tags" placeholder="Tags (separated by comma)">
-      </div>
-      <!-- Wishlist boolean -->
+    <form>
+      <div class="details-section">
+        <div class="detail">
+          <!-- Name -->
+          <label for="product-name">Name:</label>
+          <input v-model="newProduct.name" id="product-name" placeholder="name of product (required)" required>
+        </div>
+        <div class="detail">
+          <!-- Brand -->
+          <label for="brand-name">Brand:</label>
+          <input v-model="newProduct.brand" id="brand-name" placeholder="name of brand (required)" required>
+        </div>
+        <div class="detail">
+          <!-- Product type -->
+          <label for="product-type">Product type:</label>
+          <input v-model="newProduct.type" id="product-type" placeholder="type of product">
+        </div>
+        <div class="detail">
+          <!-- Product comment -->
+          <label for="product-comment">Comments:</label>
+          <input v-model="newProduct.comment" id="product-comment" placeholder="comments, notes, etc">
+        </div>
+        <div class="detail">
+          <!-- Product link -->
+          <label for="product-link">Product link:</label>
+          <input v-model="newProduct.link" id="product-link" placeholder="link to product">
+        </div>
+        <div class="detail">
+          <!-- Price cents -->
+          <label for="price">Price:</label>
+          <input v-model="newProduct.price" id="price" placeholder="price in 00.00">
+        </div>
+        <div class="detail">
+          <!-- ingredients -->
+          <label for="ingredients">Ingredients:</label>
+          <input v-model="newProduct.ingredients" id="ingredients" placeholder="ingredients separated by comma">
+        </div>
+        <div class="detail">
+          <!-- tags -->
+          <label for="tags">Tags:</label>
+          <input v-model="newProduct.tags" id="tags" placeholder="tags separated by comma">
+        </div>
+        <div class="detail long-section">
+          <!-- Wishlist boolean -->
+          <div class="checkbox-section">
+            <input v-model="wishlist" id="wishlist" type="checkbox" class="checkbox">
+            <label for="wishlist">Add product to wishlist?</label>
+          </div>
+        </div>
 
-      <!-- DEPENDENT ON WISHLIST -->
-      <!-- rating -->
-      <!-- repurchase -->
-      <!-- comment -->
-    </div>
+        <!-- Only show this section for non-wishlist products -->
+        <div v-if="!wishlist" class="detail long-section select-section">
+          <!-- Repurchase -->
+          <div>
+            <label for="repurchase">Repurchase?</label>
+            <div class="styled-select">
+              <select v-model="newProduct.repurchase" id="repurchase">
+                <option>Yes</option>
+                <option>No</option>
+                <option>Maybe</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <label for="rating">Rating:</label>
+            <div class="styled-select">
+              <select v-model="newProduct.rating" id="rating">
+                <option>0</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button v-on:click="addProduct()">Add product --></button>
+    </form>
   </div>
 </template>
 
 <script>
+import { CreateProduct } from '../graphql/mutations/create_product.graphql'
+import firebase from 'firebase'
+import 'firebase/auth'
+
 export default {
-  name: 'CreateProduct'
+  name: 'CreateProduct',
+  data () {
+    return {
+      newProduct: {
+        name: '',
+        comment: '',
+        brand: '',
+        type: '',
+        price: '0.00',
+        link: '',
+        rating: 0,
+        repurchase: 'no',
+        ingredients: [],
+        tags: []
+      },
+      wishlist: false,
+      firebaseUser: firebase.auth().currentUser.email
+    }
+  },
+  methods: {
+    addProduct () {
+      this.$apollo.mutate({
+        mutation: CreateProduct,
+        variables: {
+          name: this.newProduct.name,
+          comment: this.newProduct.comment,
+          brand: this.newProduct.brand,
+          product_type: this.newProduct.type,
+          price_cents: this.newProduct.price,
+          link: this.newProduct.link,
+          rating: String(this.newProduct.rating),
+          repurchase: String(this.newProduct.repurchase),
+          wishlist: String(this.wishlist),
+          user: this.firebaseUser
+        }
+      }).then(data => {
+        const productId = data.data.createProduct.id
+        this.$router.push({path: `/products/${productId}`})
+      })
+    }
+  }
 }
 </script>
 
@@ -56,13 +148,12 @@ export default {
 }
 
 .details-section {
+  text-align: left;
+  margin-bottom: 60px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  text-align: left;
-}
-
-.detail {
-  margin-bottom: 20px;
+  grid-column-gap: 50px;
+  grid-row-gap: 40px;
 }
 
 .detail.long-section {
@@ -79,5 +170,111 @@ input {
 label {
   font-weight: bold;
   padding-right: 5px;
+  display: block;
+}
+
+button {
+  border: 2px solid black;
+  border-radius: 0;
+  padding: 15px;
+  font-family: 'Roboto Mono', monospace;
+  font-weight: bold;
+  font-size: 14px;
+  background: transparent;
+
+  &:before {
+    content: '';
+    position: absolute;
+    height: 53px;
+    width: 160px;
+    background: #fdd6e3;
+    z-index: -10;
+    margin: -12px -22px;
+  }
+
+  &:hover {
+    &::before {
+      background: #f7adc5;
+    }
+  }
+}
+
+.checkbox-section {
+  position: relative;
+}
+
+.checkbox {
+  position: absolute;
+  opacity: 0;
+  margin-right: 15px;
+
+  & + label {
+    margin-left: 30px;
+
+    &:hover {
+      &:before {
+        background: #dedede;
+      }
+    }
+  }
+
+  & + label:before {
+    content: '';
+    position: absolute;
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background: white;
+    border: 2px solid black;
+    margin-left: -31px;
+    margin-top: -1px;
+  }
+
+   &:checked + label:after {
+    content: 'x';
+    position: absolute;
+    font-size: 20px;
+    left: 5px;
+    top: -4px;
+    width: 14px;
+    height: 14px;
+  }
+}
+
+.select-section {
+  label {
+    margin: 10px 0 5px 0;
+  }
+}
+
+.styled-select {
+  position: relative;
+  border: 2px solid black;
+  box-sizing: border-box;
+  max-width: 125px;
+
+  select {
+    border: 0 none;
+    background-color: transparent;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    width: 100%;
+    min-width: 100px;
+    padding: 0 0 6px 5px;
+  }
+
+  &:before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 7px;
+    right: 6px;
+    text-align: center;
+    pointer-events: none;
+    border-top: 6px solid black;
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+  }
 }
 </style>
