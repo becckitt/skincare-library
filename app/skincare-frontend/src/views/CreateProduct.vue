5aea69e595ce17bfe6b8c2b4cea514ac97428b04
Becca Barton <becca.barton@voxmedia.com>
2018-10-31 17:18:28 -0400

    cleanup options

--- app/skincare-frontend/src/views/CreateProduct.vue
+++ app/skincare-frontend/src/views/CreateProduct.vue
@@ -69,17 +69,7 @@
             <label for="rating">Rating:</label>
             <div class="styled-select">
               <select v-model="newProduct.rating" id="rating">
-                <option>0</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-                <option>9</option>
-                <option>10</option>
+                <option v-for="i in 10" v-bind:key="i">{{ i }}</option>
               </select>
             </div>
           </div>
