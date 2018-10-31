20b2bf5d254d72c93fb9bfe279a8779a84025aa4
Becca Barton <becca.barton@voxmedia.com>
2018-10-31 17:09:59 -0400

    cleanup

--- app/skincare-frontend/src/components/ProductsTable.vue
+++ app/skincare-frontend/src/components/ProductsTable.vue
@@ -2,8 +2,7 @@
   <div class="products-table">
     <v-client-table
       :data="products"
-      :columns="columns"
-      :options="options">
+      :columns="columns">
       <div slot="brand" slot-scope="props"><router-link :to="{ name: 'brand', params: { brandName: props.row.brand.name }}">{{ props.row.brand.name }}</router-link></div>
       <div slot="price_cents" slot-scope="props">${{ props.row.price_cents }}</div>
       <div slot="actions" slot-scope="props"><router-link :to="{name: 'product', params: { id: props.row.id }}">View</router-link></div>
